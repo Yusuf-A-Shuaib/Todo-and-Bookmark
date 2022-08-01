b02acb2e433ed642d188c1da8d7d77f6df21a54c
SHUAIB YUSUF <SHUBBY940@hotmail.com>
2022-08-01 22:45:23 +0100

    .

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#Note to start this on the 3rd of argust
-#Note to end it by 20th of argust
+#Note to start this on the 3rd of august
+#Note to end it by 20th of august
