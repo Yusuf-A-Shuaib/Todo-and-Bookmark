e6f531fb68c9d9c179af2d9998086d3de39614b3
SHUAIB YUSUF <SHUBBY940@hotmail.com>
2022-08-03 07:15:43 +0100

    new time stamp

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#Note to start this on the 3rd of august
-#Note to end it by 20th of august
+#Note to start this on the 20th of august
+#Note to end it by 10th of september
