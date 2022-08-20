6aec90636673676fbafc1adb5f26ae029e5a2303
SHUAIB YUSUF <SHUBBY940@hotmail.com>
2022-08-20 18:23:07 +0100

    Started my todo project

--- todo/__init__.py
+++ todo/__init__.py
@@ -54,10 +54,10 @@ def create_app(test_config=None):
         
     @app.errorhandler(404) 
     def error_404(e): 
-        return render_template("extras/404.html"), 404
+        return render_template("404.html"), 404
   
     @app.errorhandler(500) 
     def error_500(e): 
-        return render_template("extras/500.html"), 500
+        return render_template("500.html"), 500
 
     return app
